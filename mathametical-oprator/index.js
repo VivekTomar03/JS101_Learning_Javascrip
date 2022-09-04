9357d60b7392de521cf2fdbb2e67c02825bc0f83
vivek <tomarvivek90viv@gmail.com>
2022-09-04 14:28:39 +0530

    mathematical oprator problem

---
+++ mathametical-oprator/index.js
@@ -0,0 +1,13 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+
+let one1 = one*2;
+let two2 = two*2;
+let three3 = three*2;
+let four4 = four*2;
+let five5 = five*2;
+
+console.log(one1 + two2+ three3+ four4 + five5 )
---
+++ mathametical-oprator/problem3.js
@@ -0,0 +1,13 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+
+let two2 = two*2;
+let three3 = three*3;
+let four4 = four*3;
+let five5 = five*2;
+let six6 = six*2
+console.log(one + two2 + three3 + four4 + five5 + six6);
